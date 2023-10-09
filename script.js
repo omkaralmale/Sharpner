0d338fd0bdecc9ed97f0788e5b02281967eb59df
omkaralmale <omkaralmale@gmail.com>
2023-10-09 14:08:54 +0530

    selectorAll

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-    // Select the second item and change its background color to green
-    const secondItem = document.querySelector('#items li:nth-child(2)');
-    secondItem.style.backgroundColor = 'green';
+    // // Select the second item and change its background color to green
+    // const secondItem = document.querySelector('#items li:nth-child(2)');
+    // secondItem.style.backgroundColor = 'green';
 
-    // Select the third item and make it invisible
-    const thirdItem = document.querySelector('#items li:nth-child(3)');
-    thirdItem.style.display = 'none';
+    // // Select the third item and make it invisible
+    // const thirdItem = document.querySelector('#items li:nth-child(3)');
+    // thirdItem.style.display = 'none';
 
-    // const secondItem2=document.querySelectorAll("li");
-    // secondItem2[1].style.color='green';
+    const secondItem2=document.querySelectorAll("li");
+    secondItem2[1].style.color='green';
 
-    // const ul=document.querySelectorAll("li:nth-child(odd)");
-    // ul.forEach((li)=>{
-    //     li.style.backgroundColor='green';
-    // });
+    const ul=document.querySelectorAll("li:nth-child(odd)");
+    ul.forEach((li)=>{
+        li.style.backgroundColor='green';
+    });
 
 
