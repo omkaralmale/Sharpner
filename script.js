235420df1d2b4cbfa726514b57e476ebffad91cf
omkaralmale <omkaralmale@gmail.com>
2023-10-09 10:03:17 +0530

    changes done

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var item=document.getElementsByClassName("list-group");
 // item.forEach(element => {
 //     element.style="color:green";
 // });
+item[2].style='fontWeight=bold';
 for(let i=0; i<item.length;i++){
     item[i].style='color:red ';
 }
