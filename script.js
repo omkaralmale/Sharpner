6528b9d69cfec80c15c2f8a25840e8a60a3ca1b5
omkaralmale <omkaralmale@gmail.com>
2023-10-09 20:01:21 +0530

    upodate

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-    // // Select the second item and change its background color to green
-    // const secondItem = document.querySelector('#items li:nth-child(2)');
-    // secondItem.style.backgroundColor = 'green';
+var newDiv=document.createElement("div");
+// newDiv.innerHTML="Hello";
+newDiv.className="new module";
+newDiv.id="divNew";
+newDiv.setAttribute('title','hello div');
+//crate text node to div
+var DivText=document.createTextNode("Hello");
+//add text to div
+newDiv.appendChild(DivText);
+var title=document.querySelector("header .container");
+var h1=document.querySelector("header h1");
+title.insertBefore(newDiv,h1);
 
-    // // Select the third item and make it invisible
-    // const thirdItem = document.querySelector('#items li:nth-child(3)');
-    // thirdItem.style.display = 'none';
-
-    const secondItem2=document.querySelectorAll("li");
-    secondItem2[1].style.color='green';
-
-    const ul=document.querySelectorAll("li:nth-child(odd)");
-    ul.forEach((li)=>{
-        li.style.backgroundColor='green';
-    });
-
-
+var newDiv1=document.createElement("div");
+// newDiv.innerHTML="Hello";
+newDiv1.className="new module";
+newDiv1.id="divNew";
+newDiv1.setAttribute('title','hello div1');
+//crate text node to div
+var DivText1=document.createTextNode("Hello");
+//add text to div
+newDiv1.appendChild(DivText1);
+var title1=document.querySelector("#items");
+var li=document.querySelector(".list-group-item");
+title1.insertBefore(newDiv1,li);
